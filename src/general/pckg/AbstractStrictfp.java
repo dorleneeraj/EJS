d4a315b81b3395c6177998b8d1f0ed48e7994af5
neerajdorle <neerajdorle@gmail.com>
2017-08-14 09:12:20 +0530

    strictfp related code!

---
+++ src/general/pckg/AbstractStrictfp.java
@@ -0,0 +1,21 @@
+package general.pckg;
+
+import java.lang.reflect.*;
+
+public abstract strictfp class AbstractStrictfp {
+	public abstract void abstractMethod();
+
+	public void concreteMethod() {
+	};
+
+	public static void main(String args[]) {
+		Method methods[] = AbstractStrictfp.class.getMethods();
+		for (Method method : methods) {
+			System.out.println("Method Name::" + method.getName());
+			System.out.println("Modifiers::"
+					+ Modifier.toString(method.getModifiers()));
+			System.out.println();
+
+		}
+	}
+}
